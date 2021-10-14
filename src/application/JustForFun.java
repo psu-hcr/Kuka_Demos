0d51d59fd28aa4e3b2de720a8f234db0435051fc
bts5341 <bts5341@psu.edu>
2021-10-14 14:50:51 -0400

    --- automatic rebase to remote; old local SHA: b2243e5d5066d2a6cbff463faea5f513f6fdcd30 ---

--- src/application/JustForFun.java
+++ src/application/JustForFun.java
@@ -38,7 +38,20 @@ public class JustForFun extends RoboticsAPIApplication {
 	private LBR robot;
 	@Inject 
 	private ITaskLogger logger;
+	/* Learned Positions Table
+	 * Point 1 = First Wave Point
+	 * Point 2 = Second Wave Point
+	 * Point 3 = Unused
+	 * Point 4 = Home
+	 * Point 5 = First Fist Bump Point
+	 * Point 6 = Second Fist Bump Point
+	 */
 	Frame Point1= new Frame(329.3,-92.9,1143.7,-1.8,.8,.2);
+	Frame Point2= new Frame(334,127.6,1144.1,-1.7,-.8,.1);
+	Frame Point4= new Frame(11.7,7.4,1331.7,-1.8,-0.05,-0.01);
+	Frame Point5= new Frame(683.3,33.6,771.8,-1.6,-0.2,-1.5);
+	Frame Point6= new Frame(752.6,37.1,752.9,-1.6,-0.2,-1.5);
+	
 	LBRE1Redundancy Redundancy = new LBRE1Redundancy().setE1(0.003);
 	
 
@@ -47,6 +60,10 @@ public class JustForFun extends RoboticsAPIApplication {
 	public void initialize() {
 		// initialize your application here
 		Point1.setRedundancyInformation(robot,Redundancy);
+		Point2.setRedundancyInformation(robot,Redundancy);
+		Point4.setRedundancyInformation(robot,Redundancy);
+		Point5.setRedundancyInformation(robot,Redundancy);
+		Point6.setRedundancyInformation(robot,Redundancy);
 	}
 
 	@Override
@@ -61,12 +78,12 @@ public class JustForFun extends RoboticsAPIApplication {
 		 */
 		
 		boolean END=false;//used to mark when the user wants to leave
-		MotionBatch wave = new MotionBatch(ptp(getApplicationData().getFrame("/P1")),//sets up a MotionBatch for the waving demo
-				ptp(getApplicationData().getFrame("/P2")),//does a point to point motion to position 2
+		MotionBatch wave = new MotionBatch(ptp(Point1),//sets up a MotionBatch for the waving demo
+				ptp(Point2),//does a point to point motion to position 2
 				ptp(Point1),//does a point to point motion to position 1
-				ptp(getApplicationData().getFrame("/P2")),//does a point to point motion to position 2...
-				ptp(getApplicationData().getFrame("/P1")),
-				ptp(getApplicationData().getFrame("/P4"))).setBlendingRel(0.1).setJointVelocityRel(0.5);//BlendingRel and JointVelocityRel are used to control fluidness of motion and speed respectively
+				ptp(Point2),//does a point to point motion to position 2...
+				ptp(Point1),
+				ptp(Point4)).setBlendingRel(0.1).setJointVelocityRel(0.5);//BlendingRel and JointVelocityRel are used to control fluidness of motion and speed respectively
 		MotionBatch dance = new MotionBatch(//sets up a MotionBatch for the dancing demo
 				ptp(Math.PI/2,Math.PI/2,0,Math.PI/2,-Math.PI/2,0,0),//moves the corresponding joints to the given angles ex: joint 1 to pi/2, joint 2 to pi/2...
 				ptp(Math.PI/2,Math.PI/4,-Math.PI/4,Math.PI/4,-Math.PI/2,0,0),
@@ -85,24 +102,24 @@ public class JustForFun extends RoboticsAPIApplication {
 		
 		if (answer==0){
 			logger.info("Waving!");//prints message to Smart Pad
-			robot.move(ptp(getApplicationData().getFrame("/P4")).setJointVelocityRel(0.4));//moves to point 4
-			robot.move(ptp(getApplicationData().getFrame("/P1")).setJointVelocityRel(0.4));//moves to point 1
+			robot.move(ptp(Point4).setJointVelocityRel(0.4));//moves to point 4
+			robot.move(ptp(Point1).setJointVelocityRel(0.4));//moves to point 1
 			robot.move(wave);//does wave routine
-			robot.move(ptp(getApplicationData().getFrame("/P4")).setJointVelocityRel(0.75));//moves to point 4
+			robot.move(ptp(Point4).setJointVelocityRel(0.75));//moves to point 4
 		
 		}else if (answer==1){
 			logger.info("Fist Bump!");//prints message to Smart Pad
-			robot.move(ptp(getApplicationData().getFrame("/P4")).setJointVelocityRel(0.5));//moves to point 4
-			robot.move(ptp(getApplicationData().getFrame("/P5")).setJointVelocityRel(0.5));//moves to point 5
+			robot.move(ptp(Point4).setJointVelocityRel(0.5));//moves to point 4
+			robot.move(ptp(Point5).setJointVelocityRel(0.5));//moves to point 5
 			ThreadUtil.milliSleep(500);//waits 1/2 second
-			robot.move(ptp(getApplicationData().getFrame("/P6")).setJointVelocityRel(0.65));//moves to point 6
-			robot.move(ptp(getApplicationData().getFrame("/P5")).setJointVelocityRel(0.5));//moves to point 5
-			robot.move(ptp(getApplicationData().getFrame("/P4")).setJointVelocityRel(0.5));//moves to point 4
+			robot.move(ptp(Point6).setJointVelocityRel(0.65));//moves to point 6
+			robot.move(ptp(Point5).setJointVelocityRel(0.5));//moves to point 5
+			robot.move(ptp(Point4).setJointVelocityRel(0.5));//moves to point 4
 		}else if (answer==2){
 			logger.info("Dance!");//prints to smart pad
-			robot.move(ptp(getApplicationData().getFrame("/P4")).setJointVelocityRel(0.5));//moves to point 4
+			robot.move(ptp(Point4).setJointVelocityRel(0.5));//moves to point 4
 			robot.move(dance);//"dances"
-			robot.move(ptp(getApplicationData().getFrame("/P4")).setJointVelocityRel(0.5));//moves to point 4
+			robot.move(ptp(Point4).setJointVelocityRel(0.5));//moves to point 4
 			
 			
 			
