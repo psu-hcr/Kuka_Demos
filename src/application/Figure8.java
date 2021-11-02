26a47f0d4f3f1e99d73952edeca95607574efc65
bts5341 <bts5341@psu.edu>
2021-11-02 14:05:37 -0400

    --- automatic rebase to remote; old local SHA: 9b44abb900fbe2a26eefd4bcac9c52dd3fbc364a ---

--- src/application/Figure8.java
+++ src/application/Figure8.java
@@ -58,7 +58,7 @@ public class Figure8 extends RoboticsAPIApplication {
 	public void run() {
 		// your application execution starts here
 		robot.move(ptpHome());
-		int time = 500;
+		int time = 100;
 		double x = 0;
 		double y = 0;
 		double z = 0;
@@ -67,8 +67,8 @@ public class Figure8 extends RoboticsAPIApplication {
 		for(double i =0;i<time;i++)
 		{
 			x=600;
-			y=250*java.lang.Math.cos((2*i)/100);
-			z=100*java.lang.Math.sin((4*i)/100)+700;
+			y=250*java.lang.Math.cos((2*i)/50);
+			z=100*java.lang.Math.sin((4*i)/50)+700;
 			Frame Point= new Frame(x,y,z,0,Math.PI/2,0);
 			Points[(int)i]=Point;
 		}
