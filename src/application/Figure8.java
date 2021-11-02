a20b0cc5c1bcc52342490b64cc599bc65db06a93
bts5341 <bts5341@psu.edu>
2021-11-02 12:55:32 -0400

    --- automatic rebase to remote; old local SHA: ea9d9e275e56120554e4c72e779326560f6acd47 ---

--- src/application/Figure8.java
+++ src/application/Figure8.java
@@ -69,7 +69,7 @@ public class Figure8 extends RoboticsAPIApplication {
 		}
 		robot.move(ptpHome());
 		robot.move(ptp(.1,.1,.1,0,0,0,0));
-		Frame TestPoint=new Frame(200,200,200,0,0,0);
+		Frame TestPoint=new Frame(50,50,50,0,0,0);
 		for(int i =0;i<time;i++)
 		{
 			ptp(Points[i]);
