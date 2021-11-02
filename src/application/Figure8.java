41792dd9c42b991e02843d804060a5a0495b5e76
bts5341 <bts5341@psu.edu>
2021-11-02 14:50:01 -0400

    --- automatic rebase to remote; old local SHA: 7b6a81cf5c23708b76a5645331ec9d856ac5b2dc ---

--- src/application/Figure8.java
+++ src/application/Figure8.java
@@ -81,7 +81,7 @@ public class Figure8 extends RoboticsAPIApplication {
 	
 		for(int i =0;i<time;i++)
 		{
-			robot.moveAsync(ptp(Points[i]).setBlendingRel(1));
+			robot.moveAsync(ptp(Points[i]).setBlendingCart(10));
 		
 			
 		}
