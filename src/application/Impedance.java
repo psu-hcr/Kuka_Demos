9d5e4a1545a84d25e0669e52c346c881fa14bc05
ZACHLOGAN <92057974+ZACHLOGAN@users.noreply.github.com>
2022-07-15 09:56:30 -0400

    --- automatic rebase to remote; old local SHA: de14deef3e5676720ef86eb5f13157a80f594534 ---

--- src/application/Impedance.java
+++ src/application/Impedance.java
@@ -19,7 +19,7 @@ public class Impedance extends RoboticsAPIApplication {
 
 	private static final int stiffnessZ = 20;
 	private static final int stiffnessY = 20;
-	private static final int stiffnessX = 5000;
+	private static final int stiffnessX = 1000;
 	@Inject
 	private LBR lbr;
 	private static double[] startPosition=new double[]{0,offsetAxis2And4+Math.toRadians(30),0,offsetAxis2And4-Math.toRadians(90),0,Math.toRadians(50),0};
